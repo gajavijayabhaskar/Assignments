37a72032132bac6686a8f30447c104648267eb95
gajavijayabhaskar <50536163+gajavijayabhaskar@users.noreply.github.com>
2021-03-26 13:01:23 +0530

    Add files via upload

---
+++ Assignment_1.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1. In the below elements which of them are values or an expression? eg:- values can be integer or string and expressions will be mathematical operators.
+# Solution 1.a :    * - It is a mathematical expression used to multiply in case of integer format and if we make use of * with string it prints the string x  number of times it is mentioned as mentioned below in example.
+# Example-1:
+# a=10
+# b=10
+# print(a*b)
+# Example-2:
+# a="apple"*5
+# print(a)
+# Solution 1.b:'hello' is a string value because it is mentioned in between single quotation marks.
+# -87.8
+# Soution 1.c : -87.8 is a value belonging to float data type 
+# - 
+# Solution 1.d: - is a expression used to subtract  belonginh to mathematical operations.
+# / 
+# Solution 1.e : / is a expression used to perform mathematical operations like divison.
+# +
+# Solution 1.f: +  is a expression used to perform mathematical operations like Addition.
+# + also be used to add two strings naming it as "Concatination-operation"
+# 6 
+# Solution 1.g: 6 is a value and an integer data type  (+ve) 
+# 
+
+# 2. What is the difference between string and variable?
+# Solution 2: string is a datatype ,a group of charactes bound in between single/double quotations.
+# variable is a entity whose value is decided by the type of values we assign to it in python.variable is used to store data inorder to process it by the program.Difference between string and variable is string is convey a data/information by nature and variable is an entity used to store data /information. 
+# 
+
+# 3. Describe three different data types.
+# Solution 3: 
+# int,float -Numeric data types
+# list,tuple-collection/sequence data type 
+# string -text type 
+# dictionary -Mapping data types
+# 
+
+# 4. What is an expression made up of? What do all expressions do?
+# Solution 4:
+# An expression will make use of  the values /variable which are present in functions or classes to compile the program and exhibit an output.
+# all the expressions are used to present an output by following certain instuctions. 
+# 
+
+# 5. This assignment statements, like spam = 10. What is the difference between an expression and a statement?
+# Solution 5: Expression is a combination of values,variables ,functions and classes that helps complier to execute and exhibit a program.
+# where as statement is the next instruction to be followed in a bunch of instructions.
+# 
+
+# 6. After running the following code, what does the variable bacon contain?
+# bacon = 22
+# bacon + 1
+# Solution 6: bacon value is incremented with 1 to default 22 and final bacon value will be : 23
+# 
+
+# 7. What should the values of the following two terms be?
+# 'spam' + 'spamspam'
+# 'spam' * 3
+# Solution 7: Both the terms give same output of spamspamspam
+# 
+
+# 8. Why is eggs a valid variable name while 100 is invalid?
+# Solution 8: As per the rules and regulations in nomenclature/ naming context of python a variable can't be started with integer/float values.
+# name1-- is valid 
+# 1name -- is not valid
+# 
+
+# 9. What three functions can be used to get the integer, floating-point number, or string version of a value?
+# Solution 9: int(), float(), and str() 
+# 
+
+# 10. Why does this expression cause an error? How can you fix it?
+# 'I have eaten ' + 99 + ' burritos.'
+# Solution 10: The error would be because of concatination failure , we can't concatinate a integer with string.
+# Fix: type cast the integer 99  to string. and concatenate all of them with + operator like below ;
+# 'I have eaten ' + str(99) + ' burritos.'
+# 
